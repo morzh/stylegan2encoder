ec824b8d6737c1bd4cbfca244d8b150027fc22d4
morzhakovanton <morzh12071980>
2020-07-24 00:59:29 +0400

    increased num of iterations for initial guess

--- project_images.py
+++ project_images.py
@@ -113,7 +113,7 @@ def main():
         init_guess_file = src_file[:-3]+'npy'
         if os.path.exists(init_guess_file):
             init_guess = np.load(init_guess_file)
-            proj.num_steps = 950
+            proj.num_steps = 1550
         else:
             init_guess = np.load('stylegan2_avgface.npy')
             proj.num_steps = 2500
